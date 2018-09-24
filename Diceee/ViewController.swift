659e1c7cd7417a5138d64561b4d8e190237017e6
Josh Sample <joshuawsample@gmail.com>
2018-09-23 21:07:29 -0400

    Dice roller complete

---
+++ Diceee/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Diceee
+//
+//  Created by Joshua Sample on 9/21/18.
+//  Copyright © 2018 Joshua Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Start with random dice combo
+       updateDiceImages()
+    }
+    
+    //Button action
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    //Shake motion to roll dice - triggered automatically when motion event detected and ends
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    //Dice roll
+    func updateDiceImages() {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+
+}
+
